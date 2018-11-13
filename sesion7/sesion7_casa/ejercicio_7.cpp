e27bc237c749b44132e7b867151a4124f5b012c8
Alejandro-Villa <43407560+Alejandro-Villa@users.noreply.github.com>
2018-11-13 13:36:06 +0100

    Propuesta de código alternativo usando cstring
    Si se emplea la biblioteca cstring se simplican las entradas/salidas y se pueden usar funciones como strlen para mejorar el código. 
    Claro que también funciona sin esta biblioteca.

--- sesion7/sesion7_casa/ejercicio_7.cpp
+++ sesion7/sesion7_casa/ejercicio_7.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
-
+#include <cstring> //PROPUESTA: usar cstring para no tener que pedir longitud máxima
 using namespace std;
 
 int main(){
     const int TAMANO_MAXIMO = 10000;
     char vector[TAMANO_MAXIMO];
-    int tamano_real;
+    //int tamano_real; // Esto no sería necesario
     
     //Entrada    
-    cout << "Introduce el tamaño de la cadena: ";
-    cin >> tamano_real;
+    //cout << "Introduce el tamaño de la cadena: ";
+    //cin >> tamano_real;
 
     cout << "Introduce la cadena: ";
-    cin.ignore();
-    cin.getline(vector, tamano_real+1); //+1 por el caracter de fin de cadena
+    //cin.ignore();
+    cin.getline(vector, TAMANO_MAXIMO); //Así puedes declarar cadenas de cualquier tamaño menor que el máximo
     
     //Cálculos    
     int i = 0;
-    while (i < tamano_real){
+    while (i < strlen( vector )){
         if (vector[i] >= 'A' && vector[i] <= 'Z'){
             for (int j = i; j < tamano_real; j++){
                 vector[j] = vector[j+1];
             }
-            tamano_real--;
+           // tamano_real--; Tampoco sería necesario porque el '\0' se desplaza
         } else {
             i++;
         }
